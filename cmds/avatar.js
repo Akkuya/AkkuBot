b3661811b2979ebd7ed57cd32843a4306667aca6
Satyajit Datta <satyajitdatta03@gmail.com>
2022-01-25 09:08:56 -0500

    avatar now sends with embed

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -2,16 +2,16 @@ import { MessageEmbed } from "discord.js"
 
 export default async ({ client, message, args }) => {
     const usertofetch = await client.users.fetch(args.shift()).catch(()=>null)
-    if (!usertofetch) {message.reply("No user found!"); return}
+    if (!usertofetch) {message.reply("No user found!")}
     const useravatar = usertofetch.avatarURL()
     
     const AvEmbed = new MessageEmbed()
         .setColor('#000000')
-        .setTitle(`Avatar for @{usertofetch}`)
+        .setTitle(`Avatar for ${usertofetch.tag}`)
         .setImage(useravatar)
         
 
-    message.reply(useravatar)
+    message.reply({embeds: [AvEmbed] } )
 
 
 
