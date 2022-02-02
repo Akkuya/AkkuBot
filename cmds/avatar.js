a7cc602154586e77045659cb9fa95f611baa045f
Satyajit Datta <satyajitdatta03@gmail.com>
2022-02-01 21:29:18 -0500

    did some stuff

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,7 +1,16 @@
 import { MessageEmbed } from "discord.js"
 
 export default async ({ client, message, args }) => {
+    if (!args[0]) {
+        const selfAvEmbed = new MessageEmbed()
+            .setColor('#000000')
+            .setTitle(`Avatar for ${message.author.tag}`)
+            .setImage(message.author.avatarURL())
+        message.reply({embeds: [selfAvEmbed]})
+        return
+    }
     const usertofetch = await client.users.fetch(args.shift()).catch(()=>null)
+    
     if (!usertofetch) {message.reply("No user found!")}
     const useravatar = usertofetch.avatarURL()
     const AvEmbed = new MessageEmbed()
--- cmds/ban.js
+++ cmds/ban.js
@@ -9,16 +9,16 @@ export default async ({client, message, args}) => {
         
     const bannedUser = await client.users.fetch(userID)
         .catch(() => null);
-
     if (!bannedUser) return message.channel.send("User not found");
 
     await bannedUser.send(`You were banned from ${message.guild.name} for ${args.join(" ") ?? noReason}.`)
         .catch(() => {
             message.channel.send("User has DMs closed or has no mutual servers with the bot:(");
         });
+    const usertag = bannedUser.tag
         
     message.guild.members.ban(userID)
-        .then(banInfo => message.reply(`Banned ${banInfo.username}#${banInfo.discriminator}.`))
+        .then(banInfo => message.reply(`Banned ${usertag}.`))
         .catch(console.error);
 
     
