8c9d4f8ca2d1296879d4d24dddf9c9b2ff693167
Satyajit Datta <satyajitdatta03@gmail.com>
2022-02-27 20:05:21 -0500

    Improved DM command.

--- cmds/dm.js
+++ cmds/dm.js
@@ -1,4 +1,14 @@
-export default ({ client, args}) => {
-    client.users.cache.get(args.shift())
-        .send(args.join(' '));
+export default async ({ client, message, args }) => {
+    const recipent = await client.users.fetch(args.shift())
+        .catch(() => {
+            message.reply('User not found.');
+            return;
+        })
+
+
+    await recipent.send(`${message.author.tag} sent ${args.join(' ')}`)
+        .catch(() => { 
+            message.reply('Recipient has DM\'s closed or no mutual servers with the bot.')
+        })
+        
 }
